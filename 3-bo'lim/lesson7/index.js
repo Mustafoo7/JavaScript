29bcd4d59932acf094c7aaca346cb22b0dfed884
Mustafoo7 <eshmat028@gmail.com>
2026-07-29 21:10:10 +0500

    functoin recursion

--- 3-bo'lim/lesson7/index.js
+++ 3-bo'lim/lesson7/index.js
@@ -24,51 +24,25 @@
 
 
 
-// 11111111111111111
-// function summa(arr, i = 0) {
-//     if (i == arr.length) return 0;
 
-//     if (arr[i] % 2 == 0) {
-//         return arr[i] + summa(arr, i + 1);
-//     }
 
-//     return summa(arr, i + 1);
-// }
 
-// console.log(summa([2, 5, 8, 3, 10, 7]));
-
-// 222222222222222222222222222222222
-// function coun(str, i = 0) {
-//     if (i == str.length) return 0;
-
-//     let v = "aeiou";
-
-//     if (v.includes(str[i])) {
-//         return 1 + coun(str, i + 1);
-//     }
-
-//     return coun(str, i + 1);
-// }
-
-// console.log(coun("recursion")); 
-// 33333333333333333333333333333333333
-
-// function min(arr, i = 0) {
-//     if (i == arr.length - 1) return arr[i];
-
-//     let m = min(arr, i + 1);
-
-//     if (arr[i] < m) return arr[i];
-
-//     return m;
-// }
-
-// console.log(min([15, 3, 28, 1, 9]));\
+let str = "recurtion"
+let unli ="aeoui"
+let count = 0
+function getCount(matn){
+for(let i =0;i<matn.length;i++ ){
+    if(!unli.includes(matn[[i]]))return getCount(matn.slice(i + 1))\
+    count++
+}
+}
+getCount(str)
+console.log(count);
 
 
 
 
-// 444444444444444444444444444444444444444444444
+
 
 // function ole(n) {
 //     if (n == 0) return 0;
@@ -78,7 +52,8 @@
 
 // console.log(ole(5839));
 
-// 55555555555555555555555555555555555555555
+
+
 // function search(arr, x, i = 0) {
 //     if (i == arr.length) return false;
 
@@ -89,3 +64,18 @@
 
 // console.log(search([7, 12, 5, 9, 20], 9));
 // console.log(search([7, 12, 5, 9, 20], 15)); 
+
+
+
+// let arr = [2, 5, 8, 3, 10, 7]
+// let sum = 0
+// function getsum(data) {
+//     for (let i = 0; i < data.length; i++) {
+//         if (data[i] % 2) return getsum(data.slice(i + 1));
+//         sum += data[i];
+
+//     }
+// }
+// getsum(arr)
+// console.log(sum);
+
---
+++ 3-bo'lim/lesson8/index.js
@@ -0,0 +1,85 @@
+// Scope - {}
+
+// 1. global scope
+// 2. block scope
+// 3. functional scope
+// 111
+// let n =22
+// console.log(n);
+
+// {
+//     let  a =2
+//     console.log(a);
+    
+// }
+
+// 2222
+// {
+//     let a = 3
+
+// console.log(a);
+
+// }
+
+// 333
+// let a = 10
+// function example(){
+//     let a = 0
+
+//     console.log(a);
+    
+// }
+
+// example()
+
+
+
+//Hoisting
+
+// getsum()
+// function getsum(){
+//     console.log("Salom");
+    
+// }
+
+
+
+// console.log(m);
+
+// var m="salom"
+
+
+// clouser
+// function getdat(){
+//     let a =10 
+//     let b = 20 
+//     return ()=>{
+//         console.log(a+b);
+        
+//     }
+// }
+// getdat()()
+
+
+
+// IIFE
+// (function (){
+//     console.log("Salom");
+
+// })()
+
+// NFE
+
+// let getsum =function getsum2(){
+//     console.log("Salom");
+    
+// }
+// getsum()
+
+
+
+
+// console.log(Window); BROESER
+// console.log(global) NODE Terminal
+// console.log(globalThis);NODE JS
+
